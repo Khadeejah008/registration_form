8f70cd0b2a57666b002d5b418b65aeb11aedfaf8
khadeejah <salmann05@gmail.com>
2025-01-28 13:25:35 +0100

    registration_screen

--- lib/Screens/Registration_Screen.dart
+++ lib/Screens/Registration_Screen.dart
@@ -9,12 +9,56 @@ class _RegistrationScreenState extends State<RegistrationScreen>{
   final  TextEditingController lastnameController= TextEditingController();
   final  TextEditingController emailController= TextEditingController();
   bool isRegistered=false;
-}
+  void dispose();
+
 @override  
 Widget build(BuildContext context){
   return Scaffold(
     appBar: AppBar(
       title: Text("Registration Form"),
     ),
+    body:Padding(padding:EdgeInsets.all(20),
+    child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        TextField(controller: firstnameController,
+        decoration: InputDecoration(
+          labelText: "First Name"),
+        ),
+        TextField(controller: lastnameController,
+        decoration: InputDecoration(
+          labelText: "Last Name"),
+        ),
+        TextField(controller: emailController,
+        decoration: InputDecoration(
+          labelText: "E-mail Address"),
+        ),
+        SizedBox(height: 30,),
+        GestureDetector(
+          onTap: () {
+            setState(() {
+              isRegistered = !isRegistered ;
+            });
+          } ,
+          child: Container(
+            padding:EdgeInsets.all(20) ,
+            decoration: BoxDecoration(
+              color: isRegistered?Colors.greenAccent:Colors.blueAccent,
+              borderRadius: BorderRadius.circular(10),
+              
+            ),
+            child:Text(
+              isRegistered?"Registration Successful":"Register me",
+              
+              ),
+            ),
+          
+
+          ),
+        )
+      ],
+    ),
+    ) ,
   );
+}
 }
